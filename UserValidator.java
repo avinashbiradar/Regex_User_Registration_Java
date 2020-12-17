b51f13fdc58bf8de8b380fa97fb6b7f5dac0b02c
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-17 23:19:02 +0530

    Added:Last Name Regex Pattern

--- UserValidator.java
+++ UserValidator.java
@@ -8,13 +8,18 @@ public class UserValidator {
     }
     public static void SelectOption(){
         System.out.println("1.FirstName");
+        System.out.println("2.LastName");
         System.out.println("Enter option to check");
-        int inputOption = scan.nextInt();
 
+        int inputOption = scan.nextInt();
         switch (inputOption){
             case 1 : System.out.println("Enter your first name");
                 String firstName=scan.next();
                 firstNameValidation(firstName);
+                break;
+            case 2 : System.out.println("Enter your Last name");
+                String LastName=scan.next();
+                firstNameValidation(LastName);
                 break;
             default:
                 System.out.println("Enter a valid Option");
@@ -24,5 +29,8 @@ public class UserValidator {
         boolean valid = firstName.matches("^[A-Z]{1}[a-z]{2,}$");
         System.out.println("Name: " + firstName + "\nValid: " + valid);
     }
-
+    public static void lastNameValidation(String lastName){
+        boolean valid = lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+        System.out.println("Name: " + lastName + "\nValid: " + valid);
+    }
 }
